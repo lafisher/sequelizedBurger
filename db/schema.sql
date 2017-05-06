eea8457e291d4487fe6dd62afae709ed52aa038c
Leigh Fisher <leighfisher@Jamess-MacBook-Air-2.local>
2017-05-06 14:52:39 -0500

    initial site files

---
+++ db/schema.sql
@@ -0,0 +1,8 @@
+USE burgers_db; 
+CREATE TABLE IF NOT EXISTS burgers (
+	id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+	burger_name VARCHAR(100),
+	devoured BOOLEAN DEFAULT FALSE,
+	date TIMESTAMP
+);
+
---
+++ db/seeds.sql
@@ -0,0 +1,2 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("The L.A. Burger", FALSE), ("The Whiney Vegan", FALSE), ("The Elvis", FALSE);
